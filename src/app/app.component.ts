3e7cbb67d035b4197193cf8a559f23e0ac442495
Grant Flennoy <flennoyg@gmail.com>
2020-11-10 11:30:56 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+    },
+    {
+      title: 'Behind the scenes',
+      url: 'https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+    },
+    {
+      title: 'Breakfast of Champions',
+      url: 'https://images.unsplash.com/photo-1553849535-40d6cab25824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+    },
+    {
+      title: 'Sweet Chariot',
+      url: 'https://images.unsplash.com/photo-1591105327764-4c4b76f9e6a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+    },
+    {
+      title: 'Mans best friend',
+      url: 'https://images.unsplash.com/photo-1572963358481-26b7898cc982?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+    }
+  ];
+
+  checkWindowIndex (index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
